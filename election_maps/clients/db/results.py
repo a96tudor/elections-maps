0511d29f6be86eb589fc4318ddf3ec59a2c3bb44
Tudor Avram <a96tudor@gmail.com>
2024-06-09 00:11:14 +0300

    Add results getter by station number

--- election_maps/clients/db/results.py
+++ election_maps/clients/db/results.py
@@ -1,6 +1,14 @@
 from election_maps.clients.db.base import BaseDatabaseHandler
+from election_maps.entities.sections import VotingSection
 
 
 class ResultsDatabaseHandler(BaseDatabaseHandler):
     def __init__(self):
         super().__init__("results_collection")
+
+    def get_voting_section_by_number(self, number: str) -> VotingSection:
+        query = {"number": number}
+
+        cursor = self.find_one(query)
+
+        return VotingSection.from_dict(cursor)
